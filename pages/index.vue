22c7ae81a64714d5087eac817ad8b796965ae464
Levente26 <madi.levente26@gmail.com>
2022-06-01 13:00:21 +0200

    1/3

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,33 @@
 <template>
-  <Tutorial/>
+  <div class="container">
+    <Hero />
+    <LargeCardDisplay
+      v-for="cardInfo in largeCardInfo"
+      :key="cardInfo.id"
+      :cardSection="cardInfo"
+    />
+    <SmallCardDisplay
+      v-for="cardInfo in smallCardInfo"
+      :key="cardInfo.id"
+      :cardSection="cardInfo"
+    />
+  </div>
 </template>
 
 <script>
+import { largeCardSections } from "@/assets/data.js";
+import { smallCardSections } from "@/assets/data.js";
+
 export default {
-  name: 'IndexPage'
-}
+  name: "IndexPage",
+  data() {
+    return {
+      largeCardInfo: largeCardSections,
+      smallCardInfo: smallCardSections,
+    };
+  },
+};
 </script>
+
+<style scoped>
+</style>
